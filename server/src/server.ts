9035fc67258706ab3b6675af336b3dd5227c01f5
luan.correia <luan.correia@intelie.com.br>
2023-01-17 11:23:21 -0300

    create server

---
+++ server/src/server.ts
@@ -0,0 +1,27 @@
+import {PrismaClient} from '@prisma/client'
+import FastiFy from 'fastify'
+import cors from '@fastify/cors'
+
+const app = FastiFy()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/', async ()=>{
+    const habits = await prisma.habit.findMany({
+        where: {
+            title:{
+                startsWith: 'Beber'
+            }
+        }
+    })
+
+    return habits
+})
+
+app.listen({
+    port: 3333
+}).then(()=>{
+    console.log("HTTP server running");
+    
+})
